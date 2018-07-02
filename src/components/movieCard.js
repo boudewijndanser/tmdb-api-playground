6a6976a689abeaf82046220698e530a4ef79ce07
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-07-02 10:20:40 +0200

    Detail data comming in...

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -3,7 +3,9 @@ import React from 'react'
 const movieCard = (props) => {
     return (
         <div className="movieCard">
-        <img className="movieCardimg"src= {`${props.url}${props.poster_path}`} alt= {props.title}/>
+        <a href={`/movie/${props.id}`}>
+        <img className="movieCardimg"src= {`${props.url}${props.poster_path}`} alt= {props.title} />
+        </a>
         </div>
     )
 }
