903275819c565410bb0b0b0b439f4f6faaf1d984
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-07-06 21:04:07 +0200

    And its back!

--- src/components/movieDetail.js
+++ src/components/movieDetail.js
@@ -3,16 +3,32 @@ import React from 'react'
 const movieDetail = (props) => {
     console.log('Data from movieDetail: ',props.detailObject)
     return (
-        <div className="movieDetail">
-        <div className="baseInfo">
-        <div className=""><h1>{props.detailObject.title}</h1></div>
-        <div className=""><h2>{props.detailObject.tagline}</h2></div>
-        <div className=""><h3>{props.detailObject.release_date}</h3></div>
-        <div className=""><h4>{props.detailObject.runtime}</h4></div>
-        <div className=""><p>{props.detailObject.overview}</p></div>
-        <div className=""><p>{props.detailObject.vote_average}</p></div>
+        <div className="movieDetal">
+        <div className="singleColumn">
+        <div className="primaryContent">
+        <div className="posterLeft">
+            <img className="coverImg" src= {`${props.coverUrl}${props.detailObject.poster_path}`} alt= {props.title} width="300px" height="450px"/> 
         </div>
-        <img className="coverImg" src= {`${props.coverUrl}${props.detailObject.poster_path}`} alt= {props.title} />
+        <div className="contentRightWrapper">
+        <div className="contentRight">
+        <div className="title">
+            <h1>{props.detailObject.title}</h1>
+            </div>
+            <div className="tagline">
+            <h2>{props.detailObject.tagline}</h2>
+        </div>
+        <span className="releaseDate">
+            <h3>{props.detailObject.release_date}</h3>
+        </span>
+        
+        <div className="overviewInfo">
+            <p>{props.detailObject.overview}</p>
+        </div>
+
+        </div>
+        </div>
+        </div>
+        </div>
         </div>
     )
 }
